16a4319bc7c6e1dfa03d5f48c25014f9d2715364
Tomás Van Den Heuvel <tomasvdhcode@gmail.com>
2022-03-09 10:58:50 -0300

    Update conceptos_básicos.sol

--- Mi camino/Bloque 1 - Conceptos básicos de Solidity/Primeros pasos en Solidity/conceptos_básicos.sol
+++ Mi camino/Bloque 1 - Conceptos básicos de Solidity/Primeros pasos en Solidity/conceptos_básicos.sol
@@ -1,9 +1,14 @@
-pragma solidity >=0.4.4 <0.7.0;
+pragma solidity ^0.8.0;
+import "./ERC20.sol";
 
 contract PrimerContrato{
 
-}
+    address owner;
+    ERC20Basic token;
 
-constructor() public{
-    
+    constructor() public{
+        owner = msg.sender;
+        token = new ERC20Basic(1000);
+    }
+
 }
