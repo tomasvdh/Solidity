c9ee685aec2e668e16e46bf2a6dd2404d9e0af4a
Tomás Van Den Heuvel <tomasvdhcode@gmail.com>
2022-03-09 11:26:29 -0300

    Update conceptos_básicos.sol

--- Mi camino/Bloque 1 - Conceptos básicos de Solidity/Primeros pasos en Solidity/conceptos_básicos.sol
+++ Mi camino/Bloque 1 - Conceptos básicos de Solidity/Primeros pasos en Solidity/conceptos_básicos.sol
@@ -1,14 +1,23 @@
+// Indicar la versión
 pragma solidity ^0.8.0;
+// Importar el archivo ERC20.sol que está en nuestro directorio de trabajo
 import "./ERC20.sol";
 
+// Nuestro primer contrato
 contract PrimerContrato{
 
+    // En esta variable se encuentra la dirección de la persona que despliega el contrato
     address owner;
     ERC20Basic token;
 
+    /* 
+    Guardamos en la variable owner la dirección de la persona que despliega el contrato
+    Inicializamos el número de tokens
+    */
     constructor() public{
         owner = msg.sender;
         token = new ERC20Basic(1000);
     }
 
 }
+
