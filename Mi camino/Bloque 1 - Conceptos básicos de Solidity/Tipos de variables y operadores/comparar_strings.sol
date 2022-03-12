d5e6b72b7a1e86698546c714d2a7a876eec2cba7
Tomás Van Den Heuvel <tomasvdhcode@gmail.com>
2022-03-12 19:47:11 -0300

    Create comparar_strings.sol

---
+++ Mi camino/Bloque 1 - Conceptos básicos de Solidity/Tipos de variables y operadores/comparar_strings.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2
+
+contract comprarar_strings{
+
+    function Comparar(string memory _j, string memory _i) public returns(bool){
+
+        bytes32 hash_i = keccak256(abi.encodePacked(_i));
+        bytes32 hash_j = keccak256(abi.encodePacked(_j));
+
+        if (hash_i == hash_j){
+            returns true;
+        }
+        else{
+            returns false;
+        }
+    }
+
+}
