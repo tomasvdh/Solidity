11f773a9d2579bdf8faa93f128c129146ba6bafe
Tomás Van Den Heuvel <tomasvdhcode@gmail.com>
2022-03-14 18:09:05 -0300

    Update Modificadores.sol

--- Mi camino/Bloque 1 - Conceptos básicos de Solidity/Funciones/Modificadores.sol
+++ Mi camino/Bloque 1 - Conceptos básicos de Solidity/Funciones/Modificadores.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
 
 contract View_Pure_Payable{
 
-    // Modificador de view
+    // Modificador de View
     string[] lista_alumnos;
 
     function nuevo_alumno(string memory _alumno) public{
@@ -17,5 +18,29 @@ contract View_Pure_Payable{
     uint x=10;
     function sumarAx(uint _numero) public view returns(uint){
         return x+_numero;
+    }
+
+    // Modificador de Pure
+    function exponenciacion(uint _a, uint _b) public pure returns(uint){
+        return _a**_b;
+    }
+
+    // Modificador de Payable
+
+    mapping(address=>cartera) DineroCartera;
+
+    struct cartera{
+        address direccion_cuenta;
+        uint dinero_cuenta;
+    }
+
+    function pagar(uint _cantidad) public payable{
+        cartera memory mi_cartera;
+        mi_cartera = cartera(msg.sender, _cantidad);
+        DineroCartera[msg.sender] = mi_cartera;
+    }
+
+    function saldo() public view returns(cartera memory){
+        return DineroCartera[msg.sender];
     }
 }
