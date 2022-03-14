8f3515ab18735993b84761cff56bb3a5baae41ab
Tomás Van Den Heuvel <tomasvdhcode@gmail.com>
2022-03-14 15:59:37 -0300

    Update Returns.sol

--- Mi camino/Bloque 1 - Conceptos básicos de Solidity/Funciones/Returns.sol
+++ Mi camino/Bloque 1 - Conceptos básicos de Solidity/Funciones/Returns.sol
@@ -27,4 +27,39 @@ contract ValoresDeRetorno{
         return flag;
     }
 
+    // Función que devuelve el cociente y el residuo de una división
+    // Además devuelve una variable booleana que es true si el residuo es 0 y false en caso contrario
+    function division(uint _a, uint _b) public pure returns(uint, uint, bool){
+        uint q = _a/_b;
+        uint r = _a%_b;
+        bool multiplo;
+        if(r==0){
+            multiplo=true;
+        }
+        else{
+            multiplo=false;
+        }
+        return(q,r,multiplo);
+    }
+
+    // Práctica para el manejo de los valores devueltos
+    function numeros() public pure returns(uint, uint, uint , uint , uint, uint){
+        return (1,2,3,4,5,6);
+    }
+    // Asignación multiple
+    function todos_los_valores() pure public{
+        // Declaración de las variables donde se guardan los valores de retorno de la función numeros
+        uint a;
+        uint b;
+        uint c;
+        uint d;
+        uint e;
+        uint f;
+        // Realización de la asignación múltiple
+        (a,b,c,d,e,f)=numeros();
+    }
+    // Asignación de un subconjunto
+    function ultimo_valor() pure public{
+        (,,,,,uint ultimo)=numeros();
+    }
 }
