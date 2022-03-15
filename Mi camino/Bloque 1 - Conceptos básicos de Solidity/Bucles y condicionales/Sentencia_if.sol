905b69e2c780a170951eaa39dd7a645ec6e30572
Tomás Van Den Heuvel <tomasvdhcode@gmail.com>
2022-03-15 12:27:23 -0300

    Update Sentencia_if.sol

--- Mi camino/Bloque 1 - Conceptos básicos de Solidity/Bucles y condicionales/Sentencia_if.sol
+++ Mi camino/Bloque 1 - Conceptos básicos de Solidity/Bucles y condicionales/Sentencia_if.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
 
 contract sentencia_if{
 
@@ -30,5 +31,40 @@ contract sentencia_if{
             numero_valor_absoluto = uint(_numero);
         }
         return numero_valor_absoluto;
+    }
+
+    // Devolver true si el número introducido es par y tiene tres cifras
+    function EsParYTieneTresCirfras(uint _numero) public pure returns(bool){
+        bool ParYTresCifras;
+        if((_numero%2==0) && ((_numero/100)>=1)&&((_numero/100)<10)){
+            ParYTresCifras = true;
+        }
+        else{
+            ParYTresCifras = false;
+        }
+        return ParYTresCifras;
+    }
+
+    // Votación
+    // Solo hay tres candidatos: Joan, Gabriel y Maria
+    function Votar(string memory _candidato) public pure returns(string memory){
+        string memory mensaje;
+        if(keccak256(abi.encodePacked(_candidato))==keccak256(abi.encodePacked("Joan"))){
+            mensaje = "Has votado correctamente a Joan.";
+        }
+        else{
+            if(keccak256(abi.encodePacked(_candidato))==keccak256(abi.encodePacked("Gabriel"))){
+                mensaje = "Has votado correctamente a Gabriel.";
+            }
+            else{
+                if(keccak256(abi.encodePacked(_candidato))==keccak256(abi.encodePacked("Maria"))){
+                    mensaje = "Has votado correctamente a Maria.";
+                }
+                else{
+                    mensaje = "Has votado incorrectamente.";
+                }
+            }
+        } 
+         return mensaje;
     }
 }
