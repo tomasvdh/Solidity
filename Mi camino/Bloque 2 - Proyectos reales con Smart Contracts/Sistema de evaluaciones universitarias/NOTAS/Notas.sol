912d24a46c1fb0ac5b8b27a0d68fbed33f5eca96
Tomás Van Den Heuvel <tomasvdhcode@gmail.com>
2022-03-18 13:49:46 -0300

    Update Notas.sol

---
+++ Mi camino/Bloque 2 - Proyectos reales con Smart Contracts/Sistema de evaluaciones universitarias/NOTAS/Notas.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
+
+// -----------------------------------
+//  ALUMNO   |    ID    |      NOTA
+// -----------------------------------
+//  Marcos |    77755N    |      5
+//  Joan   |    12345X    |      9
+//  Maria  |    02468T    |      2
+//  Marta  |    13579U    |      3
+//  Alba   |    98765Z    |      5
+
+contract notas{
+
+    // Dirección del profesor
+    address public profesor;
+
+    // Constructor
+    constructor(){
+        profesor = msg.sender;
+    }
+
+    // Mapping para relacionar el hash de la identidad del alumno con su nota del examen.
+    mapping(bytes32 => uint) Notas;
+
+    // Array de los alumnos que piden revisión de examen
+    string[] revisiones;
+
+    // Eventos
+    event alumno_evaluado(bytes32, uint);
+    event evento_revision(string);
+
+    // Función evaluar a alumnos
+    function Evaluar(string memory _id_alumno, uint _nota_alumno) public UnicamenteProfesor(msg.sender){
+        // Hash de la identificación del alumno
+        bytes32 hash_id_alumno = keccak256(abi.encodePacked(_id_alumno));
+        // Relación entre el hash de la identificación del alumno y su nota
+        Notas[hash_id_alumno] = _nota_alumno;
+        // Emisión del evento
+        emit alumno_evaluado(hash_id_alumno, _nota_alumno);
+    }
+
+    modifier UnicamenteProfesor(address _direccion){
+        // Requiere que la direccion introducida por parámetro sea igual al owner del contrato
+        require(_direccion == profesor, "No tienes permisos para ejecutar esta funcion.");
+        _;
+    }
+
+
+
+ }
