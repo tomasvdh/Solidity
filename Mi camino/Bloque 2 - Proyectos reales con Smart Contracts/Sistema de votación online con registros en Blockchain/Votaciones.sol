afef6b3713076d727d5dac6d90bda606bef4cf88
Tomás Van Den Heuvel <tomasvdhcode@gmail.com>
2022-03-18 22:18:50 -0300

    Update Votaciones.sol

--- Mi camino/Bloque 2 - Proyectos reales con Smart Contracts/Sistema de votación online con registros en Blockchain/Votaciones.sol
+++ Mi camino/Bloque 2 - Proyectos reales con Smart Contracts/Sistema de votación online con registros en Blockchain/Votaciones.sol
@@ -2,8 +2,13 @@
 pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
-
-
+// -----------------------------------
+//  CANDIDATO   |   EDAD   |      ID
+// -----------------------------------
+//  Toni        |    20    |    12345X
+//  Alberto     |    23    |    54321T
+//  Joan        |    21    |    98765P
+//  Javier      |    19    |    56789W
 
 contract votacion{
 
