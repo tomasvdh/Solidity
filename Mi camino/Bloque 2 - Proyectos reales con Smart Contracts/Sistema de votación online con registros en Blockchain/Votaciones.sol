63ef709fe4775b385a0dac54021e00287931747b
Tomás Van Den Heuvel <tomasvdhcode@gmail.com>
2022-03-21 11:40:55 -0300

    Update Votaciones.sol

--- Mi camino/Bloque 2 - Proyectos reales con Smart Contracts/Sistema de votación online con registros en Blockchain/Votaciones.sol
+++ Mi camino/Bloque 2 - Proyectos reales con Smart Contracts/Sistema de votación online con registros en Blockchain/Votaciones.sol
@@ -70,24 +70,27 @@ contract votacion{
     }
 
     // Función auxiliar que transforma un uint a un string
-    function uint2str(uint _i) internal pure returns(string memory _uint2str){
-        if (_i == 0){
+    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
             return "0";
         }
         uint j = _i;
         uint len;
-        while(j != 0){
+        while (j != 0) {
             len++;
             j /= 10;
         }
         bytes memory bstr = new bytes(len);
-        uint k = len - 1;
-        while (_i != 0){
-            bstr[k--] = bytes1(uint8(48 + _i % 10));
+        uint k = len;
+        while (_i != 0) {
+            k = k-1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
             _i /= 10;
         }
         return string(bstr);
-    }
+    }   
 
     // Función para hacer un seguimiento de las votaciones
     function VerResultados() public view returns(string memory){
