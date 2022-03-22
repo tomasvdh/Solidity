777bc7b99947d87ae9cabc086d4be5c3003bc03d
Tomás Van Den Heuvel <tomasvdhcode@gmail.com>
2022-03-22 09:46:52 -0300

    Update ERC20.sol

--- Mi camino/Bloque 2 - Proyectos reales con Smart Contracts/Creación y uso de un Token ERC-20/ERC20.sol
+++ Mi camino/Bloque 2 - Proyectos reales con Smart Contracts/Creación y uso de un Token ERC-20/ERC20.sol
@@ -13,10 +13,32 @@ interface IERC20{
 
     // Función que devuelve el número de tokens que el gastador (spender) podrá gastar en nombre del propietario (owner)
     function allowance(address _owner, address _spender) external view returns(uint256);
+
+    // Función que devuelve un valor booleano con el esultado de la operación indicada
+    function trasnfer(address _recipient, uint256 _amount) external returns(bool);
+
+    // Función que devuelve un valor booleano con el resultado de la operación de gasto
+    function approve(address _spender, uint256 _amount) external returns(bool);
+
+    // Función que devuelve un valor booleano con el resultado de la operación de paso de una cantidad de tokeans usando el método allowance
+    function transferFrom(address _sender, address _recipient, uint256 _amount) external returns(bool);
+
+
+
+    // Evento que se debe emitir cuando una cantidad de tokens pase de un origen a un destino
+    event Transfer(address indexed from, address indexed to, uint256 _tokens);
+
+    // Evento que se debe emitir cuando se establece una asignación con el método allowance()
+    event Approval(address indexed _owner, address indexed _spender, uint256 _tokens)
 }
 
 // Contrato 
 contract ERC20Basic is IERC20{
+
+
+    event Transfer(address indexed from, address indexed to, uint256 _tokens);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _tokens)
+
 
     function totalSupply() public override view returns (uint256){
         return 0;
@@ -28,5 +50,17 @@ contract ERC20Basic is IERC20{
 
     function allowance(address _owner, address _spender) public override view returns(uint256){
         return 0;
+    }
+
+    function trasnfer(address _recipient, uint _amount) public override returns(bool){
+        return false;
+    }
+
+    function approve(address _spender, uint256 _amount) public override returns(bool){
+        return false;
+    }
+
+    function transferFrom(address _sender, address _recipient, uint256 _amount) public override returns(bool){
+        return false;
     }
 }
